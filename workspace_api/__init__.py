a61916462bcd35dcf874208a40e4c4c713d1ad30
mpr <mroesel@roesel.at>
2025-08-17 11:10:34 +0200

    removed unnecessary directory mount for UI_SOURCE_DIR, renamed frontend prefix to /ui

--- workspace_api/__init__.py
+++ workspace_api/__init__.py
@@ -16,8 +16,8 @@ BASE_API_DIR = os.path.dirname(os.path.abspath(__file__))
 UI_SOURCE_DIR = os.path.abspath(os.path.join(BASE_API_DIR, "..", "workspace_ui"))
 VUE_DIST_DIR = os.path.abspath(os.path.join(BASE_API_DIR, "..", "workspace_ui", "dist"))
 templates = Jinja2Templates(directory=UI_SOURCE_DIR)
-app.mount("/ui_public", StaticFiles(directory=os.path.join(UI_SOURCE_DIR, "public")), name="ui_public_assets")
-app.mount("/ui_dist", StaticFiles(directory=VUE_DIST_DIR, html=True), name="ui_dist_assets")
+# app.mount("/ui_public", StaticFiles(directory=os.path.join(UI_SOURCE_DIR, "public")), name="ui_public_assets")
+app.mount("/ui", StaticFiles(directory=VUE_DIST_DIR, html=True), name="ui_dist_assets")
 
 # TODO: set up, make sure that multiple processes are handled
 
